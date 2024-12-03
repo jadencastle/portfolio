88c0a55ca768d65dd3dc2a0c1dd71381b70258e7
jadencastle <jadencastle303@gmail.com>
2024-12-03 15:45:36 -0700

    FIRST

---
+++ portfolio/jsfinal2.js
@@ -0,0 +1,30 @@
+let subdomain = window.location.href.slice(window.location.href.lastIndexOf("/")+1, window.location.href.lastIndexOf("."));
+console.log(subdomain);
+
+fetch('../class notes/projects.json')
+    .then(response =>{
+        return response.json();
+    }).then(projects => {
+        //console.log(projects);
+        proj = projects;
+        findProjectInJSON(projects);
+        // parseData(projects);
+    }).catch(err =>{
+        console.log(`error ${err}`);
+    })
+
+function findProjectInJSON(projects){
+    for(let i=0; i<projects.projects.length; i++){
+        if(projects.projects[i].subdomain == subdomain){
+            buildPage(projects.projects[i]);
+            break;
+        }else{
+            continue;
+        }
+    }
+}
+
+function buildPage(project){
+    console.log(project);
+    document.getElementById("project").innerHTML += `<h1>${project.name}</h1>`;
+}

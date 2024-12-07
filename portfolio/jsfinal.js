5c496d9ff9628cd9e1b79cbb3ed74c0dec5e9ca1
jadencastle <jadencastle303@gmail.com>
2024-12-07 16:55:18 -0700

    FINALLY WOO!

--- portfolio/jsfinal.js
+++ portfolio/jsfinal.js
@@ -1,47 +1,64 @@
 let proj;
 fetch('/portfolio/projects.json')
-    .then(response =>{
+    .then(response => {
         return response.json();
-    }).then(projects => {
+    })
+    .then(projects => {
         console.log(projects);
         proj = projects;
         parseData(projects);
-    }).catch(err =>{
-        console.log(`error ${err}`);
     })
+    .catch(err => {
+        console.log(`error ${err}`);
+    });
 
-function parseData(data){
-    for(let i=0; i<data.projects.length; i++){
-    document.getElementById("projects").innerHTML += `<a href="/portfolio/${data.projects[i].subdomain}.html">
-    <div class="row project" id="${data.projects[i].subdomain}">
-        <div class="projimg"><img src="images/unicorns/unicorn (${i +1}).png"></div>
-        <div class="description"><h2>${data.projects[i].name}</h2><p class="subtitle">${data.projects[i].subtitle}</p>
-        <p>${data.projects[i].abstract}</p></div></div></a>`;
+function parseData(data) {
+    for (let i = 0; i < data.projects.length; i++) {
+        const project = data.projects[i];
+        const mainImage = project.mainimg; // Get the main image for each project
+        
+        let projectHtml = `
+            <a href="/portfolio/${project.subdomain}.html">
+                <div class="row project" id="${project.subdomain}">
+                    <div class="projimg">
+                        <!-- Use dynamic main image from data -->
+                        <img src="/portfolio/imgs/${mainImage}" alt="${project.name} main image">
+                    </div>
+                    <div class="description">
+                        <h2>${project.name}</h2>
+                        <p class="subtitle">${project.subtitle}</p>
+                        <p>${project.abstract}</p>
+                    </div>
+                </div>
+            </a>
+        `;
+
+        // Append projectHtml to the "projects" container
+        document.getElementById("projects").innerHTML += projectHtml;
     }
 }
 
-for(b of document.querySelectorAll("#buttons button")){
-    b.addEventListener("click", e=>{
+for (b of document.querySelectorAll("#buttons button")) {
+    b.addEventListener("click", e => {
         console.log(e.target.value);
         sortProjects(e.target.value);
-    })
+    });
 }
 
-function sortProjects(button){
-    if(button == "clear"){
-        for(let i=0; i<proj.projects.length; i++){
+function sortProjects(button) {
+    if (button == "clear") {
+        for (let i = 0; i < proj.projects.length; i++) {
             document.getElementById(proj.projects[i].subdomain).style.display = "flex";
         }
-    }else if(button != undefined){
-        for(let i=0; i<proj.projects.length;i++){
-            if(proj.projects[i].category.includes(button) == true){
+    } else if (button != undefined) {
+        for (let i = 0; i < proj.projects.length; i++) {
+            if (proj.projects[i].category.includes(button) == true) {
                 document.getElementById(proj.projects[i].subdomain).style.display = "flex";
-            }else{
+            } else {
                 document.getElementById(proj.projects[i].subdomain).style.display = "none";
             }
         }
-    }else{
+    } else {
         console.log("error, button value is undefined");
     }
-
 }
